25de2ffeaa7cd9c08b761904b956d6e1aa0eda49
Albert Miller <archalbertmiller@gmail.com>
2019-08-04 15:44:17 +0200

    Create Book class

---
+++ src/main/java/type/Book.java
@@ -0,0 +1,58 @@
+package type;
+
+public class Book {
+    private long id;
+    private String title;
+    private String author;
+    private Integer pageSum;
+    private Integer yearOfPublish;
+    private String publishingHouse;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPageSum(Integer pageSum) {
+        this.pageSum = pageSum;
+    }
+
+    public void setYearOfPublish(Integer yearOfPublish) {
+        this.yearOfPublish = yearOfPublish;
+    }
+
+    public void setPublishingHouse(String publishingHouse) {
+        this.publishingHouse = publishingHouse;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Integer getPageSum() {
+        return pageSum;
+    }
+
+    public Integer getYearOfPublish() {
+        return yearOfPublish;
+    }
+
+    public String getPublishingHouse() {
+        return publishingHouse;
+    }
+}
