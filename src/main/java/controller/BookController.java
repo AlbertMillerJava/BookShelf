a57b7f7fdec687274b6f97722bbd0bf229884960
Albert <archalbertmiller@gmail.com>
2019-09-07 10:18:32 +0200

    create posgresimpl class

--- src/main/java/controller/BookController.java
+++ src/main/java/controller/BookController.java
@@ -6,18 +6,43 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import storage.BookStorage;
 import storage.impl.StaticListBookStorageImpl;
+import type.Book;
 
-import static fi.iki.elonen.NanoHTTPD.Response.Status.INTERNAL_ERROR;
-import static fi.iki.elonen.NanoHTTPD.Response.Status.OK;
+import java.util.List;
+import java.util.Map;
+
+import static fi.iki.elonen.NanoHTTPD.Response.Status.*;
 import static fi.iki.elonen.NanoHTTPD.newFixedLengthResponse;
 
 
 public class BookController {
-
+    private final static String BOOK_ID_PARAM_NAME = "bookId";
     private BookStorage bookStorage = new StaticListBookStorageImpl();
 
     public Response serveGetBookRequest (IHTTPSession session){
-        return null;
+        Map<String, List<String>> requestParameters = session.getParameters();
+        if (requestParameters.containsKey(BOOK_ID_PARAM_NAME)){
+            List<String> bookIdParams =requestParameters.get(BOOK_ID_PARAM_NAME);
+            String bookIdParam = bookIdParams.get(0);
+            long bookId = 0;
+            try{
+                bookId = Long.parseLong(bookIdParam);
+            }catch (NumberFormatException nfe){
+                return newFixedLengthResponse(BAD_REQUEST, "text/plain", "request id ha ve to be a number");
+            }
+            Book book  = bookStorage.getBook(bookId);
+            if(book != null){
+                try{
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    String response = objectMapper.writeValueAsString(book);
+                    return newFixedLengthResponse(OK,"application/json" , response);
+                }catch (JsonProcessingException e){
+                    return newFixedLengthResponse(INTERNAL_ERROR, "text/plain","cant get all books");
+                }
+            }
+            return newFixedLengthResponse(NOT_FOUND, "application/json","");
+        }
+        return newFixedLengthResponse(BAD_REQUEST , "text/plain" , "Uncorrected request");
     }
     public Response serveGetAllBookRequest (IHTTPSession session){
         ObjectMapper objectMapper = new ObjectMapper();
@@ -31,6 +56,23 @@ public class BookController {
         return newFixedLengthResponse(OK,"application/json", response);
     }
     public Response serveAddBookRequest (IHTTPSession session){
-        return null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        long randomBookId = System.currentTimeMillis();
+        String lengthHeader = session.getHeaders().get("content-length");
+        int contentLength = Integer.parseInt(lengthHeader);
+        byte[] buffer = new byte[contentLength];
+        try{
+            session.getInputStream().read(buffer,0,contentLength);
+            String requestBody = new String(buffer).trim();
+            Book requestBook = objectMapper.readValue(requestBody,Book.class);
+            requestBook.setId(randomBookId);
+            bookStorage.addBook(requestBook);
+        }catch(Exception e){
+            System.err.println("Error during process request  \n" + e);
+            return newFixedLengthResponse(INTERNAL_ERROR, "text/plain","Internal error book hasnt been added");
+        }
+        return newFixedLengthResponse(OK,"text/plain","Book has been successfully added, id =" + randomBookId);
     }
+
+
 }
