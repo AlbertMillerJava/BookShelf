d36e511c194691d7b2f4783131252e972cbbd2fb
Albert <archalbertmiller@gmail.com>
2019-09-07 10:29:46 +0200

    create posgresimpl class

---
+++ src/main/java/storage/impl/PostgresBookStorageImpl.java
@@ -0,0 +1,22 @@
+package storage.impl;
+
+import storage.BookStorage;
+import type.Book;
+
+import java.util.List;
+
+public class PostgresBookStorageImpl implements BookStorage {
+
+
+    public Book getBook(long id) {
+        return null;
+    }
+
+    public List<Book> getAllBooks() {
+        return null;
+    }
+
+    public void addBook(Book book) {
+
+    }
+}
